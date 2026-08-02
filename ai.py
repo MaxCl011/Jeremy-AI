024fadb0fd478b013b31aa996211154dbc97f7d3
Max <maxclayton2011@gmail.com>
2026-08-02 13:53:44 +1200

    working chatbot

--- ai.py
+++ ai.py
@@ -2,24 +2,50 @@ from dotenv import load_dotenv
 import os
 from openai import OpenAI
 
-load_dotenv()
-api_key = os.getenv("NIM_api_key")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+env_path = os.path.join(script_dir, '.env')
 
-if not api_key:
+load_dotenv(dotenv_path=env_path, override=True)
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
     raise ValueError("API key not found. Please check your .env file.")
 
 client = OpenAI(
-    base_url="https://integrate.api.nvidia.com/v1",
-    api_key=api_key
+    base_url="https://api.groq.com/openai/v1",
+    api_key=GROQ_API_KEY,
+    timeout=30
 )
 
-completion = client.chat.completions.create(
-    model="meta/llama-3.3-70b-instruct",
-    messages=[{"role": "user", "content": "What is the capital of France?"}],
-    temperature=0.2,
-    top_p=0.7,
-    max_tokens=1024,
-    stream=False
-)
 
-print(completion.choices[0].message.content)
+
+def ask_question():
+
+    master_prompt = "You are a helpful assistant. talk as spongebob"
+
+    user_question = input("Whats on your mind?: ")
+
+    if user_question == "admin_mode":
+        print("Admin mode activated. You can now enter any question without restrictions.")
+        pass
+
+    print(f"User Input: {user_question}\n")
+
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "user", "content": master_prompt},
+            {"role": "user", "content": user_question}
+            ],
+        temperature=0.2,
+        top_p=0.7,
+        max_tokens=1024,
+        stream=False
+    )
+
+    print(completion.choices[0].message.content)
+
+
+while True:
+    ask_question()
+
